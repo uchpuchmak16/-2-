4f5425a192aedf4e63d27587edff03f7ab9187f4
User <User@DESKTOP-2HTTQT5>
2023-11-04 14:50:49 +0300

    Добавьте файлы проекта.

---
+++ enc_temp_folder/a3b39a56cef46831b2e4777931d897/ConsoleApplication4.cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm> /*делаю через итераторы, поэтому добавляю эту бибилиотеку*/
+#include <map>
+
+int main() {
+    setlocale(LC_ALL, "Rus");
+    std::vector<std::string> names = { "Иволгин Роман", "Петров Николай", "Лебедева Мария", "Вологодская Елена", "Семенов Андрей", "Тимощук Валентин", "Окладов Сергей", "Яковлев Лев", "Балконская Юлия", "Ахматова Анастасия" };
+    std::vector<int> marks;
+
+    for (int i = 0; i < names.size(); i++) {
+        int mark;
+        std::cout << "введите оценку студента от 0 до 100 " << names[i] << ": ";
+        std::cin >> mark;
+        marks.push_back(mark); /*добавление элемента в вектор с оценками */
+    }
+
+    // мода
+    std::map<int, int> countMap;
+    for (int i = 0; i < marks.size(); i++) {
+        countMap[marks[i]]++;
+    }
+    int maxcount = 0;
+    int mostfrequentmark = -1;
+    for (auto it = countMap.begin(); it != countMap.end(); it++) {
+        if (it->second > maxcount) {
+            maxcount = it->second;
+            mostfrequentmark = it->first;
+        }
+    }
+
+    // средний балл
+    double sum = 0;
+    for (int i = 0; i < marks.size(); i++) {
+        sum += marks[i];
+    }
+    double averagemark = sum / marks.size();
+
+    // сортируем оценки
+    std::sort(marks.begin(), marks.end());
+
+    //  медиана
+    double median;
+    if (marks.size() % 2 == 0) {
+        median = (marks[marks.size() / 2 - 1] + marks[marks.size() / 2]) / 2.0;
+    }
+    else {
+        median = marks[marks.size() / 2];
+    }
+
+    std::cout << "студенты с оценкой " << mostfrequentmark << ":" << std::endl;
+    for (int i = 0; i < marks.size(); i++) {
+        if (marks[i] == mostfrequentmark) {
+            std::cout << names[i] << std::endl;
+        }
+    }
+    for (int i = 0; i < names.size(); i++) {
+        int mark;
+        std::cout << "введите оценку студента от 0 до 100, чтобы убрать ее " << names[i] << ": ";
+        std::cin >> mark;
+        marks.push_back(mark); /*добавление элемента в вектор с оценками */
+    }
+
+    std::cout << "мода: " << mostfrequentmark << std::endl;
+    std::cout << "средний балл: " << averagemark << std::endl;
+    std::cout << "медиана: " << median << std::endl;
+
+    return 0;
+}
